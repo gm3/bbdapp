7c614b28006b059eb6fc687e813f6a147aedbf23
boomboxhead <gm3beats@gmail.com>
2022-12-19 04:22:46 -0700

    init Playcanvas

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1618971.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 115567208, 115567187, 115567212, 115567215, 115567221, 115567088, 115567134, 115567225, 115567186, 115567234, 115567069, 115567119, 115567135, 115566959, 115566944, 115566934, 115567174, 115567070, 115567209, 115567172, 115567210, 115567178, 115566941, 115566974, 115567195, 115567263, 115567251, 115566929, 115567193, 115567179, 115567322, 115570481, 115571749, 115583203, 115585203 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/115567177/1/basis.wasm.js', 'wasmUrl' : 'files/assets/115566967/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/115567264/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/115567192/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/115567041/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/115567185/1/ammo.js', 'preload' : true},
+];
